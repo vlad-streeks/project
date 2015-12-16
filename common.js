409ae7526e29a33ebbcb98ba9a7deac5f9a29e65
vlad-streeks <streeks4352@gmail.com>
2015-12-16 17:49:22 +0200

    finish

--- common.js
+++ common.js
@@ -1,3 +1,34 @@
 
 	new WOW().init();
 
+$(document).ready(function() {
+  $(".gamburger").click(function (event) { 
+
+	$(".nav ul").toggleClass("open"); 
+})
+
+  //Попап менеджер FancyBox
+	//Документация: http://fancybox.net/howto
+	//<a class="fancybox"><img src="image.jpg" /></a>
+	//<a class="fancybox" data-fancybox-group="group"><img src="image.jpg" /></a>
+	$(".fancybox").fancybox();
+
+	//Каруселька
+	//Документация: http://owlgraphic.com/owlcarousel/
+	var owl = $(".carousel");
+	owl.owlCarousel({
+		items : 2,
+		autoPlay: true
+	});
+	
+	$(".next_button").click(function(){
+		owl.trigger("owl.next");
+	});
+	$(".prev_button").click(function(){
+		owl.trigger("owl.prev");
+	});
+
+
+
+
+});
